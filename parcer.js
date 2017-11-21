78d3bace17c6a3adccf20c45a9973bb2570eb65d
evbe0615 <evgenii.belobragin@netcracker.com>
2017-11-21 15:21:48 +0300

    simple parcer

---
+++ parcer.js
@@ -0,0 +1,44 @@
+var tress = require('tress');
+var needle = require('needle');
+var cheerio = require('cheerio');
+var resolve = require('url').resolve;
+var fs = require('fs');
+var rambler_news_url = 'https://news.rambler.ru';
+var moscow = rambler_news_url+'/moscow_city/?updated';
+var voronezh = rambler_news_url+'/Voronezh/?updated';
+var results = [];
+//var hrefs =[];
+
+var q = tress(function(url, callback){
+    needle.get(url, function(err, res){
+        if (err) throw err;
+
+        // парсим DOM
+        var $ = cheerio.load(res.body);
+
+        var news = $('.top-topic__news-item');
+        news.each(function(i, element){
+            var content = $(element).children();
+            var href = rambler_news_url+$(content).attr('href');
+            var imageRef = $($($($(content).children()[0]).children()[0]).children()[0]).attr('data-src');
+            var topic = $($($($(content).children()[1]).children()[0]).children()[0]).text();
+
+            if(href != undefined && topic != undefined && imageRef != undefined){
+                console.log('----------------------');
+                console.log('topic: '+topic);
+                console.log('imageRef: '+imageRef);
+                console.log('href: '+href);
+            }
+
+        });
+        
+
+        callback();
+    });
+}, 1); // запускаем 10 параллельных потоков
+
+q.drain = function(){
+    fs.writeFileSync('./data.json', JSON.stringify(results, null, 4));
+}
+
+q.push(voronezh);
